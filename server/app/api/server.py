078b3f1101236f9183d83c67cd657b272ced1e06
Prakshit0902 <s.prakshit2004@gmail.com>
2026-05-17 17:15:13 +0530

    cors issue fixed for server

--- server/app/api/server.py
+++ server/app/api/server.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI, UploadFile, BackgroundTasks
 from fastapi.responses import StreamingResponse
 
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 
 from app.qa.stream_ask import stream_question
@@ -14,6 +15,15 @@ from app.api.upload import (
 
 
 app = FastAPI(title="PDF RAG API", version="1.0.0")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 
 class ChatRequest(BaseModel):
